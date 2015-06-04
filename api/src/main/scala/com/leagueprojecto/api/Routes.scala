6a631227c0fb9ec3edcaae5554106d839cf70a0c
Martin Kanters <mkanters93@gmail.com>
2015-06-04 21:38:32 +0200

    Routes en Startup van elkaar gescheiden

---
+++ api/src/main/scala/com/leagueprojecto/api/Routes.scala
@@ -0,0 +1,87 @@
+package com.leagueprojecto.api
+
+import akka.actor.{ActorSystem, ActorRef}
+import akka.event.LoggingAdapter
+import akka.http.model.HttpResponse
+import akka.http.model.StatusCodes._
+import akka.http.model.headers.RawHeader
+import akka.http.server.Directives._
+import akka.http.server.ExceptionHandler
+import akka.pattern.ask
+import akka.util.Timeout
+import com.leagueprojecto.api.domain.{MatchHistory, Summoner}
+import com.leagueprojecto.api.services.CacheService.CachedResponse
+import com.leagueprojecto.api.services.riot.MatchHistoryService.GetMatchHistory
+import com.leagueprojecto.api.services.riot.SummonerService.GetSummonerByName
+import com.leagueprojecto.api.services.riot.{RiotService, SummonerService}
+import com.typesafe.config.Config
+import akka.http.marshallers.sprayjson.SprayJsonSupport._
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.duration._
+
+trait Routes extends JsonProtocols {
+  implicit val system: ActorSystem
+  implicit val executor: ExecutionContextExecutor
+  implicit val timeout: Timeout = Timeout(5.seconds)
+
+  def config: Config
+  val logger: LoggingAdapter
+
+  val cachedSummonerService: ActorRef
+  val cachedMatchHistoryService: ActorRef
+
+  def regionMatcher = config.getString("riot.regions").r
+
+  val optionsSupport = {
+    options {
+      complete("")
+    }
+  }
+
+  implicit def myExceptionHandler = ExceptionHandler {
+    case e: SummonerService.SummonerNotFound  => complete(HttpResponse(NotFound))
+    case e: RiotService.ServiceNotAvailable   => complete(HttpResponse(ServiceUnavailable))
+    case e: RiotService.TooManyRequests       => complete(HttpResponse(TooManyRequests))
+    case _                                    => complete(HttpResponse(InternalServerError))
+  }
+
+  val corsHeaders = List(RawHeader("Access-Control-Allow-Origin", "*"),
+    RawHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE"),
+    RawHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"))
+
+  def summonerRoute(implicit region: String) = {
+    pathPrefix("summoner" / Segment) { name =>
+      pathEndOrSingleSlash {
+        get {
+          complete {
+            (cachedSummonerService ? GetSummonerByName(region, name)).mapTo[CachedResponse[Summoner]]
+          }
+        } ~ optionsSupport
+      }
+    }
+  }
+
+  def matchhistoryRoute(implicit region: String) = {
+    pathPrefix("matchhistory" / LongNumber) { summonerId =>
+      pathEndOrSingleSlash {
+        get {
+          complete {
+            (cachedMatchHistoryService ? GetMatchHistory(region, summonerId)).mapTo[CachedResponse[List[MatchHistory]]]
+          }
+        } ~ optionsSupport
+      }
+    }
+  }
+
+
+  val routes = {
+    //  logRequestResult("API-service") {
+    respondWithHeaders(corsHeaders) {
+      pathPrefix("api" / regionMatcher) { regionSegment =>
+        implicit val region = regionSegment.toLowerCase
+
+        summonerRoute ~ matchhistoryRoute
+      }
+    }
+  }
+}
--- api/src/main/scala/com/leagueprojecto/api/Startup.scala
+++ api/src/main/scala/com/leagueprojecto/api/Startup.scala
@@ -3,37 +3,19 @@ package com.leagueprojecto.api
 import akka.actor._
 import akka.event.Logging
 import akka.http.Http
-import akka.http.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.model.HttpResponse
-import akka.http.model.StatusCodes._
-import akka.http.model.headers.RawHeader
-import akka.http.server.Directives._
-import akka.http.server.ExceptionHandler
 import akka.stream.ActorFlowMaterializer
-import akka.util.Timeout
 import com.leagueprojecto.api.domain.{MatchHistory, Summoner}
 import com.leagueprojecto.api.services.CacheService
-import CacheService.CachedResponse
-import com.leagueprojecto.api.services.riot.MatchHistoryService.GetMatchHistory
-import com.leagueprojecto.api.services.riot.{MatchHistoryService, RiotService, SummonerService}
-import com.leagueprojecto.api.services.riot.SummonerService.GetSummonerByName
+import com.leagueprojecto.api.services.riot.{MatchHistoryService, SummonerService}
 import com.typesafe.config.ConfigFactory
 
-import akka.pattern.ask
+object Startup extends App with Routes {
+  override implicit val system: ActorSystem = ActorSystem("api")
+  override implicit val executor = system.dispatcher
+  override val config = ConfigFactory.load()
+  override val logger = Logging(system, getClass)
 
-import scala.concurrent.duration._
-
-object Startup extends App with JsonProtocols {
-  implicit val system = ActorSystem("api")
-  implicit val timeout: Timeout = 5.seconds
-
-  implicit val executor = system.dispatcher
   implicit val materializer = ActorFlowMaterializer()
-
-  val config = ConfigFactory.load()
-  val logger = Logging(system, getClass)
-
-  val regionMatcher = config.getString("riot.regions").r
 
   // Services
   val summonerService: ActorRef = system.actorOf(SummonerService.props)
@@ -42,62 +24,10 @@ object Startup extends App with JsonProtocols {
   // Service caches
   val summonerCacheTime = config.getLong("riot.services.summonerbyname.cacheTime")
   val matchhistoryCacheTime = config.getLong("riot.services.matchhistory.cacheTime")
-  val cachedSummonerService: ActorRef     = system.actorOf(CacheService.props[Summoner](summonerService, summonerCacheTime))
-  val cachedMatchHistoryService: ActorRef = system.actorOf(CacheService.props[List[MatchHistory]](matchHistoryService, matchhistoryCacheTime))
-
-  val optionsSupport = {
-    options {
-      complete("")
-    }
-  }
-
-  implicit def myExceptionHandler = ExceptionHandler {
-    case e: SummonerService.SummonerNotFound  => complete(HttpResponse(NotFound))
-    case e: RiotService.ServiceNotAvailable   => complete(HttpResponse(ServiceUnavailable))
-    case e: RiotService.TooManyRequests       => complete(HttpResponse(TooManyRequests))
-    case _                                    => complete(HttpResponse(InternalServerError))
-  }
-
-  val corsHeaders = List(RawHeader("Access-Control-Allow-Origin", "*"),
-    RawHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE"),
-    RawHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"))
-
-  def summonerRoute(implicit region: String) = {
-    pathPrefix("summoner" / Segment) { name =>
-      pathEndOrSingleSlash {
-        get {
-          complete {
-            (cachedSummonerService ? GetSummonerByName(region, name)).mapTo[CachedResponse[Summoner]]
-        }
-        } ~ optionsSupport
-      }
-    }
-  }
-
-  def matchhistoryRoute(implicit region: String) = {
-    pathPrefix("matchhistory" / LongNumber) { summonerId =>
-      pathEndOrSingleSlash {
-        get {
-          complete {
-            (cachedMatchHistoryService ? GetMatchHistory(region, summonerId)).mapTo[CachedResponse[List[MatchHistory]]]
-          }
-        } ~ optionsSupport
-      }
-    }
-  }
-
-
-  val routes = {
-  //  logRequestResult("API-service") {
-      respondWithHeaders(corsHeaders) {
-        pathPrefix("api" / regionMatcher) { regionSegment =>
-          implicit val region = regionSegment.toLowerCase
-
-          summonerRoute ~
-          matchhistoryRoute
-        }
-      }
-  }
+  override val cachedSummonerService: ActorRef =
+    system.actorOf(CacheService.props[Summoner](summonerService, summonerCacheTime))
+  override val cachedMatchHistoryService: ActorRef =
+    system.actorOf(CacheService.props[List[MatchHistory]](matchHistoryService, matchhistoryCacheTime))
 
   // Bind the HTTP endpoint. Specify http.interface and http.port in the configuration
   // to change the address and port to bind to.
