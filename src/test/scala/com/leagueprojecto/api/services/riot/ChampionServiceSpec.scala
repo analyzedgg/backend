e8515daeb27119cd2ae2ebc0187ffedcbab7ed91
Koen <koen.vd@hotmail.com>
2016-07-27 17:52:46 +0200

    WiP for tests

--- src/test/scala/com/leagueprojecto/api/services/riot/ChampionServiceSpec.scala
+++ src/test/scala/com/leagueprojecto/api/services/riot/ChampionServiceSpec.scala
@@ -1,10 +1,14 @@
 package com.leagueprojecto.api.services.riot
 
 import akka.actor.ActorSystem
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.testkit.{ImplicitSender, TestActorRef, TestKit}
 import org.scalatest.{GivenWhenThen, Matchers, WordSpecLike}
 
 class ChampionServiceSpec extends TestKit(ActorSystem("ChampionServiceTest")) with ImplicitSender with WordSpecLike with Matchers with GivenWhenThen {
+
+  val sut = TestActorRef(new ChampionService{
+
+  })
 
   "A ChampionService" must {
 
