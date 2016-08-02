364a9335ce78aaf9b3e39c9a273c0dc8c34e1c94
Koen <koen.vd@hotmail.com>
2016-08-02 18:17:43 +0200

    Try and fix travis build

--- src/test/scala/com/leagueprojecto/api/testHelpers/BaseServiceHelper.scala
+++ src/test/scala/com/leagueprojecto/api/testHelpers/BaseServiceHelper.scala
@@ -7,6 +7,9 @@ import org.scalatest.{GivenWhenThen, Matchers, WordSpecLike}
 import scala.io.Source
 
 abstract class BaseServiceHelper(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with Matchers with GivenWhenThen {
-  def readFileFromClasspath(file: String): String =
-    Source.fromURL(getClass.getResource(file)).mkString
+  def readFileFromClasspath(file: String): String = {
+    val path = getClass.getResource("/responses")
+    println(path)
+    Source.fromInputStream(getClass.getResourceAsStream(file)).mkString
+  }
 }
