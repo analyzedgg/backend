3184e620ef14f23b2c023cbb0a28bdb348260fbb
Koen <koen.vd@hotmail.com>
2016-08-02 19:23:11 +0200

    Fix the path to the test json file

--- src/test/scala/com/leagueprojecto/api/testHelpers/BaseServiceHelper.scala
+++ src/test/scala/com/leagueprojecto/api/testHelpers/BaseServiceHelper.scala
@@ -8,8 +8,10 @@ import scala.io.Source
 
 abstract class BaseServiceHelper(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with Matchers with GivenWhenThen {
   def readFileFromClasspath(file: String): String = {
+    val path = getClass.getResource("/responses")
+    println(s"Test resources response folder path: $path")
     val filePath = getClass.getResource(file)
-    println(filePath)
+    println(s"Test resouces json file path: $filePath")
     val fileStream = getClass.getResourceAsStream(file)
     Source.fromInputStream(fileStream).mkString
   }
