3306b3a3f65879e345aea71d784e11d40d7fc888
Koen <koen.vd@hotmail.com>
2016-08-02 19:26:59 +0200

    Remove debugging info

--- src/test/scala/com/leagueprojecto/api/testHelpers/BaseServiceHelper.scala
+++ src/test/scala/com/leagueprojecto/api/testHelpers/BaseServiceHelper.scala
@@ -7,12 +7,5 @@ import org.scalatest.{GivenWhenThen, Matchers, WordSpecLike}
 import scala.io.Source
 
 abstract class BaseServiceHelper(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with Matchers with GivenWhenThen {
-  def readFileFromClasspath(file: String): String = {
-    val path = getClass.getResource("/responses")
-    println(s"Test resources response folder path: $path")
-    val filePath = getClass.getResource(file)
-    println(s"Test resouces json file path: $filePath")
-    val fileStream = getClass.getResourceAsStream(file)
-    Source.fromInputStream(fileStream).mkString
-  }
+  def readFileFromClasspath(file: String): String = Source.fromInputStream(getClass.getResourceAsStream(file)).mkString
 }
