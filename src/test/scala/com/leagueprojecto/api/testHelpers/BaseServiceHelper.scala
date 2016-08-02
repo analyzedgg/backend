af08ac673b2cb37b973ec9e33b308c377880c2f3
Koen <koen.vd@hotmail.com>
2016-08-02 18:23:25 +0200

    Add extra "debugging"

--- src/test/scala/com/leagueprojecto/api/testHelpers/BaseServiceHelper.scala
+++ src/test/scala/com/leagueprojecto/api/testHelpers/BaseServiceHelper.scala
@@ -8,8 +8,9 @@ import scala.io.Source
 
 abstract class BaseServiceHelper(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with WordSpecLike with Matchers with GivenWhenThen {
   def readFileFromClasspath(file: String): String = {
-    val path = getClass.getResource("/responses")
-    println(path)
-    Source.fromInputStream(getClass.getResourceAsStream(file)).mkString
+    val filePath = getClass.getResource(file)
+    println(filePath)
+    val fileStream = getClass.getResourceAsStream(file)
+    Source.fromInputStream(fileStream).mkString
   }
 }
